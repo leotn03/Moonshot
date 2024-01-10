91047880f5c9c4da32364d856f2bb58faa9bb88f
leotn03 <leo.tn03@icloud.com>
2024-01-10 13:36:01 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Leo Torres Neyra on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
